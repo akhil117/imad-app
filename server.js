2c173c1d2c8477d0c463113166865758f1d33b3d
Akhil <akhilbatchu123@gmail.com>
2017-08-04 22:14:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/name', function (req, res) {
-  res.sen  d(createTemplate(Content));
+  res.send(createTemplate(Content));
 });
 
 
