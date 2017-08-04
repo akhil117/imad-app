d5ae45d0e4f027f7d18008a49c5c34750059903d
Akhil <akhilbatchu123@gmail.com>
2017-08-04 18:34:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,18 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/article-1', function(req,res){
+    res.send("this is an article 1");
+});
+
+app.get('/article-2',function(req,res){
+    res.send("this is article 2 ")
+    
+});
+app.get('/article-3',function(req,res){
+    res.send("this is article 2 ")
+    
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
