865b79cbe0d35c9e998a5f53f0417204f53bb98f
Akhil <akhilbatchu123@gmail.com>
2017-09-20 18:16:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,10 +6,10 @@ var img = document.getElementById('boma');
 img.onclick = function(){
     img.style.marginRight="100px" ;
 };
-var count=0;
+
 function m()
 {
-      count = count + 1; 
+    var  count = count + 1; 
       console.log(count);
     document.getElementById("counter").innerHTML = count.toString();
     
