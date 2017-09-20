369069f6aab094b3e1779b6ef3c5f26a57d433b4
Akhil <akhilbatchu123@gmail.com>
2017-09-20 18:23:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,4 @@
-console.log('Loaded!');
 
-var chan = document.getElementById('edit');
-chan.innerHTML = "good morning";
-var img = document.getElementById('boma');
-img.onclick = function(){
-    img.style.marginRight="100px" ;
-};
 var counter = 0;
 function m()
 {
